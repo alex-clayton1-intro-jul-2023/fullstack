22f21a7f14104ce4345855ce2f9543d1793c4b9e
Alex Clayton <alexander_clayton@progressive.com>
2023-07-17 16:02:09 -0400

    safe spot to commit

--- frontend/app/src/app/components/header.component.ts
+++ frontend/app/src/app/components/header.component.ts
@@ -5,9 +5,9 @@ import { Component } from "@angular/core";
     standalone: true,
     selector: 'app-header', // Can do whatever here
     template: `
-<header>
-<h1 class=>Intro to Programming</h1>
-<p>Sample Full-Stack Application for testing!! so fun!</p>
+<header class="p-4 border-b-2 border-black">
+<h1 class="text-4xl text-primary font-black">Intro to Programming</h1>
+<p>Sample Full-Stack Application For The Training Class</p>
 </header>
     `
 })
