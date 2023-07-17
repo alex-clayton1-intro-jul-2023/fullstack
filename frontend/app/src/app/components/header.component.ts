9ba4cc9e8db8b4fcccbeb7935c5fa76f481868af
Alex Clayton <alexander_clayton@progressive.com>
2023-07-17 14:24:34 -0400

    Initial

---
+++ frontend/app/src/app/components/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+ 
+@Component({
+    standalone: true,
+    selector: 'app-header', // Can do whatever here
+    template: `
+<header>
+<h1>Intro to Programming</h1>
+<p>Sample Full-Stack Application</p>
+</header>
+    `
+})
+export class HeaderComponent {
+
+ 
+
+}
