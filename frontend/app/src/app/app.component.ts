8b67a403bd06836908f4153c70f0607b66298760
Alex Clayton <alexander_clayton@progressive.com>
2023-07-18 10:25:45 -0400

    extra comments

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NavigationComponent } from "./components/navigation.component";
 
 @Component({
     selector: 'app-root',
-    standalone: true,
+    standalone: true, // Angular 2 - 15 (16) didn't have this. You had to have a thing called an "Angular Module" that each component belonged to. Awkward + confusing. Currently on Angular 16 and doing standalone components is now the most recent thing
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     imports: [CommonModule, RouterOutlet, HeaderComponent, NavigationComponent]
