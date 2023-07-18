54f150733a6f1eb703adf3c9112a99874917938d
Alex Clayton <alexander_clayton@progressive.com>
2023-07-18 13:54:46 -0400

    Lunch stopping point. Whole bunch of cyprus tests

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -5,11 +5,11 @@ import { HeaderComponent } from "./components/header.component";
 import { NavigationComponent } from "./components/navigation.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true, // Angular 2 - 15 (16) didn't have this. You had to have a thing called an "Angular Module" that each component belonged to. Awkward + confusing. Currently on Angular 16 and doing standalone components is now the most recent thing
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, NavigationComponent]
+  selector: 'app-root',
+  standalone: true, // Angular 2 - 15 (16) didn't have this. You had to have a thing called an "Angular Module" that each component belonged to. Awkward + confusing. Currently on Angular 16 and doing standalone components is now the most recent thing
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NavigationComponent]
 })
 export class AppComponent {
   title = 'Intro to Programming Sample App.'; // Controls the name of the webpage
